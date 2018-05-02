1a3975b57679ff903d26209df31e2efe19951e85
AdamSJohnson <Adam.Johnson.main@gmail.com>
2018-05-01 22:14:03 -0400

    Added a notified list as to not be alerted by the same message multiple times

--- Parse.py
+++ Parse.py
@@ -10,11 +10,19 @@ push = PushBullet(API_KEY)
 devices = push.getDevices()
 phone = devices[1]["iden"]
 
-
-
-check = []
+check = open('CheckedAlready.txt', 'r')
+dontnotifylist = []
+for line in check:
+    dontnotifylist.append(line.replace('\n',''))
+check.close()
+print(dontnotifylist)
 subreddit = reddit.subreddit('boardgamedeals')
 for post in subreddit.new(limit=25):
-    if(  'gloomhaven' in post.title.lower() ) and not post.id in check:
+    if(  'gloomhaven' in post.title.lower() ) and not post.id in dontnotifylist:
         print(post.title)
-        check.append(post.id)
+        dontnotifylist.append(post.id)
+
+write = ''
+for line in dontnotifylist:
+    write = write + line + '\n'
+open('CheckedAlready.txt', 'w').write(write)
